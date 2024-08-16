c8601ce66d5820fff680011472562c564683a71f
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-16 19:51:17 +0700

    Add Header component to Layout

--- frontend/src/App.tsx
+++
@@ -1,6 +0,0 @@
-import './App.css'
-import { Button } from './components/ui/button'
-
-export default function App() {
-	return <Button>Click Me!</Button>
-}
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 
-import './index.css'
+import './global.css'
 import AppRoutes from './AppRoutes.tsx'
 
 createRoot(document.getElementById('root')!).render(
