445b66a7369bcd7f1c5c3d0f3fdccf8da4ded319
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-17 15:41:47 +0700

    Validate access token

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -7,11 +7,13 @@ type Props = {
 
 export default function Auth0ProviderWithNavigate({ children }: Props) {
 	const navigate = useNavigate()
+
 	const domain = import.meta.env.VITE_AUTH0_DOMAIN
 	const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 	const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
+	const audience = import.meta.env.VITE_AUTH0_AUDIENCE
 
-	if (!domain || !clientId || !redirectUri) {
+	if (!domain || !clientId || !redirectUri || !audience) {
 		throw new Error('Unable to initialise auth')
 	}
 
@@ -25,6 +27,7 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
 			clientId={clientId}
 			authorizationParams={{
 				redirect_uri: redirectUri,
+				audience,
 			}}
 			onRedirectCallback={onRedirectCallback}
 		>
