142320f4f48760551ed4d47b863c651754ac2afe
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-17 11:30:16 +0700

    Add authenticated user to MobileNav

--- frontend/src/components/MobileNav.tsx
+++ frontend/src/components/MobileNav.tsx
@@ -1,5 +1,7 @@
-import { Menu } from 'lucide-react'
+import { useAuth0 } from '@auth0/auth0-react'
+import { CircleUserRound, Menu } from 'lucide-react'
 
+import MobileNavLinks from './MobileNavLinks'
 import { Separator } from './ui/separator'
 import { Button } from './ui/button'
 import {
@@ -11,6 +13,8 @@ import {
 } from './ui/sheet'
 
 export default function MobileNav() {
+	const { isAuthenticated, loginWithRedirect, user } = useAuth0()
+
 	return (
 		<Sheet>
 			<SheetTrigger>
@@ -19,11 +23,27 @@ export default function MobileNav() {
 
 			<SheetContent className='space-y-3'>
 				<SheetTitle>
-					<span> Welcome to MernEats.com!</span>
+					{isAuthenticated ? (
+						<span className='flex items-center font-bold gap-2'>
+							<CircleUserRound className='text-orange-500' />
+							{user?.email}
+						</span>
+					) : (
+						<span> Welcome to MernEats.com!</span>
+					)}
 				</SheetTitle>
 				<Separator />
 				<SheetDescription className='flex flex-col gap-4'>
-					<Button>Log In</Button>
+					{isAuthenticated ? (
+						<MobileNavLinks />
+					) : (
+						<Button
+							onClick={() => loginWithRedirect()}
+							className='flex-1 font-bold bg-orange-500'
+						>
+							Log In
+						</Button>
+					)}
 				</SheetDescription>
 			</SheetContent>
 		</Sheet>
