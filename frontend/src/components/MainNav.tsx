f133803fec1cfc44ab0a797402026734d82baf61
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-17 11:10:44 +0700

    Add authenticated user to MainNav

--- frontend/src/components/MainNav.tsx
+++ frontend/src/components/MainNav.tsx
@@ -1,19 +1,24 @@
 import { useAuth0 } from '@auth0/auth0-react'
 
+import UsernameMenu from './UsernameMenu'
 import { Button } from './ui/button'
 
 export default function MainNav() {
-	const { loginWithRedirect } = useAuth0()
+	const { loginWithRedirect, isAuthenticated } = useAuth0()
 
 	return (
 		<span className='flex space-x-2 items-center'>
-			<Button
-				className='font-bold hover:text-orange-500 hover:bg-white'
-				variant='ghost'
-				onClick={async () => await loginWithRedirect()}
-			>
-				Log In
-			</Button>
+			{isAuthenticated ? (
+				<UsernameMenu />
+			) : (
+				<Button
+					className='font-bold hover:text-orange-500 hover:bg-white'
+					variant='ghost'
+					onClick={async () => await loginWithRedirect()}
+				>
+					Log In
+				</Button>
+			)}
 		</span>
 	)
 }
