b5d1b37b0f2873bc2099211ce4f09d44ea7354d1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-17 14:23:34 +0700

    Create user request (Sign Up and Log In) via client

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,10 +1,14 @@
 import { AppState, Auth0Provider } from '@auth0/auth0-react'
 
+import { useCreateMyUser } from '@/api/MyUserApi'
+
 type Props = {
 	children: React.ReactNode
 }
 
 export default function Auth0ProviderWithNavigate({ children }: Props) {
+	const { createUser } = useCreateMyUser()
+
 	const domain = import.meta.env.VITE_AUTH0_DOMAIN
 	const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID
 	const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL
@@ -14,7 +18,9 @@ export default function Auth0ProviderWithNavigate({ children }: Props) {
 	}
 
 	const onRedirectCallback = (appState?: AppState, user?: User) => {
-		console.log('USER', user)
+		if (user?.sub && user?.email) {
+			createUser({ auth0Id: user.sub, email: user.email })
+		}
 	}
 
 	return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,17 +1,28 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
 
 import './global.css'
-import AppRoutes from './AppRoutes.tsx'
 import Auth0ProviderWithNavigate from './auth/Auth0ProviderWithNavigate.tsx'
+import AppRoutes from './AppRoutes.tsx'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false,
+		},
+	},
+})
 
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<Router>
-			<Auth0ProviderWithNavigate>
-				<AppRoutes />
-			</Auth0ProviderWithNavigate>
+			<QueryClientProvider client={queryClient}>
+				<Auth0ProviderWithNavigate>
+					<AppRoutes />
+				</Auth0ProviderWithNavigate>
+			</QueryClientProvider>
 		</Router>
 	</StrictMode>
 )
