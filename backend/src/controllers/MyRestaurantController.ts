acafdb90bf1f3b51cb015edef5cad5a727d790b1
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-18 14:47:38 +0700

    Add restaurant API request: getMyRestaurant

--- backend/src/controllers/MyRestaurantController.ts
+++ backend/src/controllers/MyRestaurantController.ts
@@ -4,6 +4,21 @@ import cloudinary from 'cloudinary'
 
 import Restaurant from '../models/restaurant'
 
+const getMyRestaurant = async (req: Request, res: Response) => {
+	try {
+		const restaurant = await Restaurant.findOne({ user: req.userId })
+
+		if (!restaurant) {
+			return res.status(404).json({ message: 'restaurant not found' })
+		}
+
+		res.json(restaurant)
+	} catch (error) {
+		console.log('error', error)
+		res.status(500).json({ message: 'Error fetching restaurant' })
+	}
+}
+
 const createMyRestaurant = async (req: Request, res: Response) => {
 	try {
 		const existingRestaurant = await Restaurant.findOne({ user: req.userId })
@@ -32,5 +47,6 @@ const createMyRestaurant = async (req: Request, res: Response) => {
 }
 
 export default {
+	getMyRestaurant,
 	createMyRestaurant,
 }
--- backend/src/routes/MyRestaurantRoute.ts
+++ backend/src/routes/MyRestaurantRoute.ts
@@ -13,6 +13,8 @@ const upload = multer({
 	},
 })
 
+router.get('/', jwtCheck, jwtParse, MyRestaurantController.getMyRestaurant)
+
 router.post(
 	'/',
 	jwtCheck,
