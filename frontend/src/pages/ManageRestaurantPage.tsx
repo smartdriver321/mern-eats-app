c1015d8250f29933c497411e2c6ca7086d40b6bd
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-18 19:10:46 +0700

    Add restaurant API request: useUpdateMyRestaurant

--- frontend/src/pages/ManageRestaurantPage.tsx
+++ frontend/src/pages/ManageRestaurantPage.tsx
@@ -1,20 +1,24 @@
 import {
 	useCreateMyRestaurant,
 	useGetMyRestaurant,
+	useUpdateMyRestaurant,
 } from '@/api/MyRestaurantApi'
 import ManageRestaurantForm from '@/forms/manage-restaurant-form/ManageRestaurantForm'
 
 export default function ManageRestaurantPage() {
 	const { restaurant } = useGetMyRestaurant()
-
 	const { createRestaurant, isLoading: isCreateLoading } =
 		useCreateMyRestaurant()
+	const { updateRestaurant, isLoading: isUpdateLoading } =
+		useUpdateMyRestaurant()
+
+	const isEditing = !!restaurant
 
 	return (
 		<ManageRestaurantForm
 			restaurant={restaurant}
-			onSave={createRestaurant}
-			isLoading={isCreateLoading}
+			onSave={isEditing ? updateRestaurant : createRestaurant}
+			isLoading={isCreateLoading || isUpdateLoading}
 		/>
 	)
 }
