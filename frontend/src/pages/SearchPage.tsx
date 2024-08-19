767bd3afbda64ae9795125ba78d186eef69cf13b
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-19 10:24:58 +0700

    Create restaurant API request: useSearchRestaurants

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,7 +1,21 @@
 import { useParams } from 'react-router-dom'
 
+import { useSearchRestaurants } from '@/api/RestaurantApi'
+
 export default function SearchPage() {
 	const { city } = useParams()
-
-	return <span>User searched for {city}</span>
+	const { results } = useSearchRestaurants(city)
+	console.log(results)
+	return (
+		<span>
+			User searched for {city}
+			<span>
+				{results?.data.map((restaurant) => (
+					<span>
+						found {restaurant.restaurantName},{restaurant.city}
+					</span>
+				))}
+			</span>
+		</span>
+	)
 }
