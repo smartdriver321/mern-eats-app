c50e8e16557a02a8cec2bf73a5241e5654c18474
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-19 11:56:01 +0700

    Implement search filter to SearchPage

--- frontend/src/api/RestaurantApi.tsx
+++ frontend/src/api/RestaurantApi.tsx
@@ -1,13 +1,23 @@
 import { useQuery } from 'react-query'
 
 import { RestaurantSearchResponse } from '@/types'
+import { SearchState } from '@/pages/SearchPage'
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL
 
-export const useSearchRestaurants = (city?: string) => {
+export const useSearchRestaurants = (
+	searchState: SearchState,
+	city?: string
+) => {
 	const createSearchRequest = async (): Promise<RestaurantSearchResponse> => {
+		const params = new URLSearchParams()
+		params.set('searchQuery', searchState.searchQuery)
+		params.set('page', searchState.page.toString())
+		params.set('selectedCuisines', searchState.selectedCuisines.join(','))
+		params.set('sortOption', searchState.sortOption)
+
 		const response = await fetch(
-			`${API_BASE_URL}/api/restaurant/search/${city}`
+			`${API_BASE_URL}/api/restaurant/search/${city}?${params.toString()}`
 		)
 
 		if (!response.ok) {
@@ -18,7 +28,7 @@ export const useSearchRestaurants = (city?: string) => {
 	}
 
 	const { data: results, isLoading } = useQuery(
-		['searchRestaurants'],
+		['searchRestaurants', searchState],
 		createSearchRequest,
 		{ enabled: !!city }
 	)
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,12 +1,44 @@
+import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import SearchBar, { SearchForm } from '@/components/SearchBar'
 import SearchResultInfo from '@/components/SearchResultInfo'
 import SearchResultCard from '@/components/SearchResultCard'
 
+export type SearchState = {
+	searchQuery: string
+	page: number
+	selectedCuisines: string[]
+	sortOption: string
+}
+
 export default function SearchPage() {
+	const [searchState, setSearchState] = useState<SearchState>({
+		searchQuery: '',
+		page: 1,
+		selectedCuisines: [],
+		sortOption: 'bestMatch',
+	})
+
 	const { city } = useParams()
-	const { results, isLoading } = useSearchRestaurants(city)
+	const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+	const setSearchQuery = (searchFormData: SearchForm) => {
+		setSearchState((prevState) => ({
+			...prevState,
+			searchQuery: searchFormData.searchQuery,
+			page: 1,
+		}))
+	}
+
+	const resetSearch = () => {
+		setSearchState((prevState) => ({
+			...prevState,
+			searchQuery: '',
+			page: 1,
+		}))
+	}
 
 	if (!results?.data || !city) {
 		return <span>No results found</span>
@@ -20,6 +52,12 @@ export default function SearchPage() {
 		<div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
 			<div id='cuisines-list'>Insert cuisines here</div>
 			<div id='main-content' className='flex flex-col gap-5'>
+				<SearchBar
+					searchQuery={searchState.searchQuery}
+					onSubmit={setSearchQuery}
+					placeHolder='Search by Cuisine or Restaurant Name'
+					onReset={resetSearch}
+				/>
 				<div className='flex justify-between flex-col gap-3 lg:flex-row'>
 					<SearchResultInfo total={results.pagination.total} city={city} />
 				</div>
