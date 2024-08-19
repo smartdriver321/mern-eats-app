df35d37cfbfe5d128c28dd4f76b48536826707ee
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-19 19:47:10 +0700

    Update createCheckoutSession for creating order

--- backend/src/controllers/OrderController.ts
+++ backend/src/controllers/OrderController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import Stripe from 'stripe'
 
 import Restaurant, { MenuItemType } from '../models/restaurant'
+import Order from '../models/order'
 
 const STRIPE = new Stripe(process.env.STRIPE_API_KEY as string)
 const FRONTEND_URL = process.env.FRONTEND_URL as string
@@ -98,6 +99,15 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 			throw new Error('Restaurant not found')
 		}
 
+		const newOrder = new Order({
+			restaurant: restaurant,
+			user: req.userId,
+			status: 'placed',
+			deliveryDetails: checkoutSessionRequest.deliveryDetails,
+			cartItems: checkoutSessionRequest.cartItems,
+			createdAt: new Date(),
+		})
+
 		const lineItems = createLineItems(
 			checkoutSessionRequest,
 			restaurant.menuItems
@@ -105,7 +115,7 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 
 		const session = await createSession(
 			lineItems,
-			'test_order_id',
+			newOrder._id.toString(),
 			restaurant.deliveryPrice,
 			restaurant._id.toString()
 		)
@@ -113,6 +123,8 @@ const createCheckoutSession = async (req: Request, res: Response) => {
 		if (!session.url) {
 			return res.status(500).json({ message: 'Error creating stripe session' })
 		}
+
+		await newOrder.save()
 
 		res.json({ url: session.url })
 	} catch (error: any) {
