d93797ab8292225097779367e8d562bf25540ac5
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-19 13:33:24 +0700

    Implement cuisine filtering to SearchPage

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import { useParams } from 'react-router-dom'
 
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import CuisineFilter from '@/components/CuisineFilter'
 import SearchBar, { SearchForm } from '@/components/SearchBar'
 import SearchResultInfo from '@/components/SearchResultInfo'
 import SearchResultCard from '@/components/SearchResultCard'
@@ -15,6 +16,8 @@ export type SearchState = {
 }
 
 export default function SearchPage() {
+	const [isExpanded, setIsExpanded] = useState<boolean>(false)
+
 	const [searchState, setSearchState] = useState<SearchState>({
 		searchQuery: '',
 		page: 1,
@@ -24,6 +27,14 @@ export default function SearchPage() {
 
 	const { city } = useParams()
 	const { results, isLoading } = useSearchRestaurants(searchState, city)
+
+	const setSelectedCuisines = (selectedCuisines: string[]) => {
+		setSearchState((prevState) => ({
+			...prevState,
+			selectedCuisines,
+			page: 1,
+		}))
+	}
 
 	const setSearchQuery = (searchFormData: SearchForm) => {
 		setSearchState((prevState) => ({
@@ -58,7 +69,16 @@ export default function SearchPage() {
 
 	return (
 		<div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
-			<div id='cuisines-list'>Insert cuisines here</div>
+			<div id='cuisines-list'>
+				<CuisineFilter
+					selectedCuisines={searchState.selectedCuisines}
+					onChange={setSelectedCuisines}
+					isExpanded={isExpanded}
+					onExpandedClick={() =>
+						setIsExpanded((prevIsExpanded) => !prevIsExpanded)
+					}
+				/>
+			</div>
 
 			<div id='main-content' className='flex flex-col gap-5'>
 				<SearchBar
@@ -67,6 +87,7 @@ export default function SearchPage() {
 					placeHolder='Search by Cuisine or Restaurant Name'
 					onReset={resetSearch}
 				/>
+
 				<div className='flex justify-between flex-col gap-3 lg:flex-row'>
 					<SearchResultInfo total={results.pagination.total} city={city} />
 				</div>
