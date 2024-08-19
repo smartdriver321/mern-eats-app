225a07d395851c332210f5ac84059e2013ee3a6a
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-19 10:57:12 +0700

    Add SearchResultInfo component to SearchPage

--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,21 +1,28 @@
 import { useParams } from 'react-router-dom'
 
 import { useSearchRestaurants } from '@/api/RestaurantApi'
+import SearchResultInfo from '@/components/SearchResultInfo'
 
 export default function SearchPage() {
 	const { city } = useParams()
-	const { results } = useSearchRestaurants(city)
-	console.log(results)
+	const { results, isLoading } = useSearchRestaurants(city)
+
+	if (!results?.data || !city) {
+		return <span>No results found</span>
+	}
+
+	if (isLoading) {
+		;<span>Loading ...</span>
+	}
+
 	return (
-		<span>
-			User searched for {city}
-			<span>
-				{results?.data.map((restaurant) => (
-					<span>
-						found {restaurant.restaurantName},{restaurant.city}
-					</span>
-				))}
-			</span>
-		</span>
+		<div className='grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5'>
+			<div id='cuisines-list'>Insert cuisines here</div>
+			<div id='main-content' className='flex flex-col gap-5'>
+				<div className='flex justify-between flex-col gap-3 lg:flex-row'>
+					<SearchResultInfo total={results.pagination.total} city={city} />
+				</div>
+			</div>
+		</div>
 	)
 }
