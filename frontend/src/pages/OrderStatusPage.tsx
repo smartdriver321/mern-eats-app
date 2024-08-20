3990d3b500eb33a68130e8fab6a04e5245bf93ac
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-20 12:13:22 +0700

    Add OrderStatusHeader component to OrderStatusPage

--- frontend/src/pages/OrderStatusPage.tsx
+++ frontend/src/pages/OrderStatusPage.tsx
@@ -1,6 +1,7 @@
 import { AspectRatio } from '@/components/ui/aspect-ratio'
 
 import { useGetMyOrders } from '@/api/OrderApi'
+import OrderStatusHeader from '@/components/OrderStatusHeader'
 
 export default function OrderStatusPage() {
 	const { orders, isLoading } = useGetMyOrders()
@@ -17,7 +18,7 @@ export default function OrderStatusPage() {
 		<div className='space-y-10'>
 			{orders.map((order) => (
 				<div className='space-y-10 bg-gray-50 p-10 rounded-lg'>
-					OrderStatusHeader
+					<OrderStatusHeader order={order} />
 					<div className='grid gap-10 md:grid-cols-2'>
 						OrderStatusDetail
 						<AspectRatio ratio={16 / 5}>
