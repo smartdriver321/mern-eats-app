94a9d8cccb14c8bb056fe1f8daf5edd785de05f3
smartdriver321 <tinggaldidisneyland@gmail.com>
2024-08-20 12:26:41 +0700

    Add Order Status and Manage Restaurant link to MobileNavLink

--- frontend/src/components/MobileNavLinks.tsx
+++ frontend/src/components/MobileNavLinks.tsx
@@ -9,12 +9,23 @@ export default function MobileNavLinks() {
 	return (
 		<>
 			<Link
+				to='/order-status'
+				className='flex bg-white items-center font-bold hover:text-orange-500'
+			>
+				Order Status
+			</Link>
+			<Link
+				to='/manage-restaurant'
+				className='flex bg-white items-center font-bold hover:text-orange-500'
+			>
+				Manage Restaurant
+			</Link>
+			<Link
 				to='/user-profile'
 				className='flex bg-white items-center font-bold hover:text-orange-500'
 			>
 				User Profile
 			</Link>
-
 			<Button
 				onClick={() => logout()}
 				className='flex items-center px-3 font-bold hover:bg-gray-500'
